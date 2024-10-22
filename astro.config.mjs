4e40ab231642131f8b5eca1ecf4e2a4e5de488e7
zacktagnan <zacktagnan@gmail.com>
2024-10-22 17:56:15 +0200

    Rutas.estAticas.dinAmicas-Modos.de.renderizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+
+  // con SERVER, automáticamente, todo se convierte en SSR o dinámico
+  // output: 'server',
+  // con HYBRID, habrá que desactivar el prerender para que esa parte sea dinámica
+  output: 'hybrid',
 });
